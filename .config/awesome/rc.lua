2b1384e291c13f64e0cf1e0061099756979ea292
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-06-15 14:59:17 +0200

    Even better keys

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -250,50 +250,52 @@ globalkeys = awful.util.table.join(
     awful.key({ altkey }, "Right", function () lain.util.tag_view_nonempty(1) end,
               {description = "view  previous nonempty", group = "tag"}),
 
-    -- Default client focus
-    awful.key({ altkey,           }, "j",
-        function ()
-            awful.client.focus.byidx( 1)
-        end,
-        {description = "focus next by index", group = "client"}
-    ),
-    awful.key({ altkey,           }, "k",
-        function ()
-            awful.client.focus.byidx(-1)
-        end,
-        {description = "focus previous by index", group = "client"}
-    ),
-    awful.key({ altkey,           }, "l",
-        function ()
-            awful.client.focus.byidx(-1)
-        end,
-        {description = "focus previous by index", group = "client"}
-    ),
-
     -- By direction client focus
-    awful.key({ modkey }, "j",
-        function()
-            awful.client.focus.bydirection("down")
-            if client.focus then client.focus:raise() end
-        end),
     awful.key({ modkey }, "k",
         function()
-            awful.client.focus.bydirection("up")
+            awful.client.focus.global_bydirection("down")
             if client.focus then client.focus:raise() end
         end),
-    awful.key({ modkey }, "h",
+    awful.key({ modkey }, "i",
         function()
-            awful.client.focus.bydirection("left")
+            awful.client.focus.global_bydirection("up")
+            if client.focus then client.focus:raise() end
+        end),
+    awful.key({ modkey }, "j",
+        function()
+            awful.client.focus.global_bydirection("left")
             if client.focus then client.focus:raise() end
         end),
     awful.key({ modkey }, "l",
         function()
-            awful.client.focus.bydirection("right")
+            awful.client.focus.global_bydirection("right")
             if client.focus then client.focus:raise() end
         end),
     awful.key({ modkey,           }, "w", function () awful.util.mymainmenu:show() end,
               {description = "show main menu", group = "awesome"}),
-
+ 
+ 
+  -- By direction screen focus
+    awful.key({ altkey }, "k",
+        function()
+            awful.screen.focus_bydirection("down")
+            if client.focus then client.focus:raise() end
+        end),
+    awful.key({ altkey }, "i",
+        function()
+            awful.screen.focus_bydirection("up")
+            if client.focus then client.focus:raise() end
+        end),
+    awful.key({ altkey }, "j",
+        function()
+            awful.screen.focus_bydirection("left")
+            if client.focus then client.focus:raise() end
+        end),
+    awful.key({ altkey }, "l",
+        function()
+            awful.screen.focus_bydirection("right")
+            if client.focus then client.focus:raise() end
+        end),
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end,
               {description = "swap with next client by index", group = "client"}),
